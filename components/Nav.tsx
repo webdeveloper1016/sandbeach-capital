444980ce7b449510abd88fe7aed4aff26a9eca3f
Bryan Grill <grillb@gmail.com>
2020-11-26 18:39:22 -0500

    add some packages

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,32 +1,28 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
-const links = [
-  { href: '/about', label: 'About' },
-]
+const links = [{ href: '/portfolio', label: 'Portfolio' }];
 
-export default function Nav() {
-  return (
-    <nav>
-      <ul className="flex items-center justify-between p-8">
-        <li>
-          <Link href="/">
-            <a className="text-blue-500 no-underline text-accent-1 dark:text-blue-300">
-              Home With Typescript
-            </a>
-          </Link>
-        </li>
-        <ul className="flex items-center justify-between space-x-4">
-          {links.map(({ href, label }) => (
-            <li key={`${href}${label}`}>
-              <Link href={href}>
-              <a  className="no-underline btn-blue">
-                {label}
-              </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
+const Nav = () => (
+  <nav>
+    <ul className="flex items-center justify-between p-8">
+      <li>
+        <Link href="/">
+          <a className="text-blue-500 no-underline text-accent-1 dark:text-blue-300">
+            Home With Typescript
+          </a>
+        </Link>
+      </li>
+      <ul className="flex items-center justify-between space-x-4">
+        {links.map(({ href, label }) => (
+          <li key={`${href}${label}`}>
+            <Link href={href}>
+              <a className="no-underline btn-blue">{label}</a>
+            </Link>
+          </li>
+        ))}
       </ul>
-    </nav>
-  )
-}
+    </ul>
+  </nav>
+);
+
+export default Nav;
