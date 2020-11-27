56adadcce8567a7354d882d7c38019f224338520
Bryan Grill <grillb@gmail.com>
2020-11-26 20:04:49 -0500

    start build out data model

--- pages/api/portfolio.ts
+++ pages/api/portfolio.ts
@@ -1,16 +1,32 @@
-const data = [
-  { category: 'short-term' },
-  { category: 'long-term' },
+import { PortfolioModel } from '../../types';
+
+const data: PortfolioModel[] = [
+  { category: 'short-term', sector: 'Cash', amount: 13000 },
+  {
+    category: 'short-term',
+    sector: 'Robo',
+    amount: 2000,
+    pie: [
+      { slice: 'US stock', percentage: 0.5 },
+      { slice: 'Us Govt bond', percentage: 0.5 },
+    ],
+  },
+  {
+    category: 'long-term',
+    sector: 'M1',
+    amount: 20,
+    pie: [{ slice: 'Hedge fund follow', percentage: 0.1 }],
+  },
   {
     category: 'retirement',
-    sector: 'US Stocks',
-    subSector: 'tech',
-    style: 'ETF',
-    ticker: 'QQQ',
-    fees: 0.2,
-    shares: 133,
+    sector: 'US Stock',
+    subSector: 'Tech stock',
+    // style: 'ETF',
+    // ticker: 'QQQ',
+    // fees: 0.2,
+    // shares: 133,
     amount: 38000,
-    yield: 0.55
+    // yield: 0.55,
   },
 ];
 
--- types.ts
+++ types.ts
@@ -1,3 +1,50 @@
+export type SectorType =
+  | 'US Stock'
+  | 'Intl Stock'
+  | 'US Bond'
+  | 'Intl Bond'
+  | 'Alternative'
+  | 'ETF'
+  | 'Mutual Fund'
+  | 'Robo'
+  | 'M1'
+  | 'Cash';
+
+export type SubSectorType =
+  | 'US stock'
+  | 'Tech stock'
+  | 'International stock'
+  | 'EMG market stock'
+  | 'Us Govt bond'
+  | 'Foreign Bond'
+  | 'EMG bond'
+  | 'Corporate bond'
+  | 'Hedge fund'
+  | 'Hedge fund follow'
+  | 'Private equity'
+  | 'VC'
+  | 'Pre IPO'
+  | 'Income share'
+  | 'Real estate'
+  | 'Reit'
+  | 'Art'
+  | 'Currency'
+  | 'Crypto'
+  | 'Commodity'
+  | 'Cash';
+
 export interface PortfolioModel {
-  category: "short-term" | "long-term" | "retirement"
+  category: 'short-term' | 'long-term' | 'retirement';
+  sector: SectorType;
+  subSector?: SubSectorType;
+  pie?: { slice: SubSectorType; percentage: number }[];
+  amount: number
 }
+
+// style: 'ETF',
+// strategy: cash, income, value, growth, hedge, alt
+// ticker: 'QQQ',
+// fees: 0.2,
+// shares: 133,
+// amount: 38000,
+// yield: 0.55
