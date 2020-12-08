4e65f710594e16f13e3af5b1d0ef812d023c6c58
Bryan Grill <grillb@gmail.com>
2020-12-08 14:32:07 -0500

    set doc title

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,13 @@
+import Head from 'next/head';
 import Nav from './Nav';
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
     <div>
+      <Head>
+        <title>Portfolio Manager</title>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
       <Nav />
       <div className="container px-10 py-5">{children}</div>
     </div>
