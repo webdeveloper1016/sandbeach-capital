9ffca223040052f2ebe0d7ce80ee5613b0321128
Bryan Grill <grillb@gmail.com>
2020-12-28 05:44:20 -0500

    build url for batch quotes

--- data/watchlist.ts
+++ data/watchlist.ts
@@ -1,5 +1,6 @@
 export const watchlist = {
-  growth: ['BYND', 'DOCU', 'TSLA'],
+  growth: ['BYND', 'DOCU', 'TSLA', 'TWLO', 'SPLK'],
   value: ['WFC', 'BAC', 'LMT', 'PSA'],
   like: ['AAPL', 'TGT', 'CMCSA', 'WMT', 'HD', 'BLK'],
+  radar: ['JOET']
 };
--- utils/fetch.ts
+++ utils/fetch.ts
@@ -5,14 +5,16 @@ export const fetcher = (url) =>
     (res) => res.json() as Promise<{ data: PortfolioModel; token: string }>,
   );
 
-//https://intercom.help/iexcloud/en/articles/2852094-how-do-i-query-multiple-symbols-or-data-types-in-one-api-call
 export const fetchQuotes = (
   tickers: TickerModel,
   token: string,
 ): Promise<any> => {
-  const iex = `https://sandbox.iexapis.com/stable/stock/IBM/quote?token=${token}`;
+  const symbols = tickers.stock.join(',');
+  const batch = `https://sandbox.iexapis.com/v1/stock/market/batch?types=quote&symbols=${symbols}&token=${token}`;
+  // const iex = `https://sandbox.iexapis.com/stable/stock/IBM/quote?token=${token}`;
   console.log(tickers);
-  return fetch(iex).then((res) => {
+  console.log(batch);
+  return fetch(batch).then((res) => {
     const data = res.json();
     return data;
   });
