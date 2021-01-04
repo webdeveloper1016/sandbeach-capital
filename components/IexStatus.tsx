72c684d37775ea6c34975e87d9d57147255e1fd0
Bryan Grill <grillb@gmail.com>
2021-01-04 17:36:50 -0500

    add initial skeleton

--- components/IexStatus.tsx
+++ components/IexStatus.tsx
@@ -4,7 +4,7 @@ const IexStatus = (): React.ReactElement => {
   const { data, updatedAt, status} = useFetchPortfolio();
   console.log(data, updatedAt);
   if (status !== 'success') {
-    return <li>skeleton</li>
+    return <li className=" animate-pulse h-4 bg-green-500 rounded w-1/4"></li>
   }
   return <li>{data.iex.env} Updated 1 Min ago</li>;
 };
