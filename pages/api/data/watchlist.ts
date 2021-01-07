af15aec333fa70cb8c21bafa8c2e81109e3139ea
Bryan Grill <grillb@gmail.com>
2021-01-07 15:29:33 -0500

    update watchlist

--- pages/api/data/watchlist.ts
+++ pages/api/data/watchlist.ts
@@ -1,6 +1,6 @@
 export const watchlist = {
-  potentialAdditions: [],
+  potentialAdditions: ['IPOE', 'CLOV', 'SPCE', 'PENN'],
   growth: ['CRWD', 'PLAN', 'NOW', 'SPLK'],
   incomeValue: [],
-  radar: ['JOET', 'KOMP', 'XT', 'UFO, space custom pie?']
+  radar: ['JOET', 'KOMP', 'XT', 'BUG', 'UFO, space custom pie?'],
 };
