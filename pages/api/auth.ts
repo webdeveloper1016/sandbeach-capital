3482c366bf45fcb3ae899705c0e640902485e340
Bryan Grill <grillb@gmail.com>
2021-01-08 17:14:25 -0500

    add auth route

---
+++ pages/api/auth.ts
@@ -0,0 +1,47 @@
+const prod = process.env.NODE_ENV === 'production';
+
+interface ReqModel {
+  method: 'POST' | 'GET';
+  body: {
+    secret: string;
+  };
+}
+const handler = (req: ReqModel, res) => {
+  if (req.method === 'POST') {
+    try {
+      const body = req.body;
+      // check for body
+      if (!body.secret) {
+        res.status(400).send('Bad Request');
+        return
+      }
+
+      // validate token
+      if (body.secret !== process.env.APP_SECRET) {
+        res.status(401).send('Unauthorized');
+        return
+      }
+
+      // gen token and send it back
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(
+        JSON.stringify({ token: '1234'}),
+      );
+    } catch (error) {
+      res.status(500).send(
+        prod
+          ? 'Server Error'
+          : JSON.stringify({
+              status: 'Server Error',
+              message: error.message,
+            }),
+      );
+    }
+  } else {
+    // Handle any other HTTP method
+    res.status(405).send('Unsupported Method!');
+  }
+};
+
+export default handler;
