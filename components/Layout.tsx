5e20f8e41fd73925898ca2c6149eff47719cad61
Bryan Grill <grillb@gmail.com>
2021-01-11 23:13:11 -0500

    mv head to _app

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,12 @@
-import Head from 'next/head';
 import Nav from './Nav';
 
 const Layout = ({ children }) => {
   return (
     <div>
-      <Head>
-        <title>Sand Beach Capital</title>
-        <link rel="shortcut icon" href="/favicon.png" />
-      </Head>
       <Nav />
-      <div className="container p-4 md:px-8 lg:px-20 container-extended">{children}</div>
+      <div className="container p-4 md:px-8 lg:px-20 container-extended">
+        {children}
+      </div>
     </div>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,21 @@
+import Head from 'next/head';
 import '../styles/index.css';
 import { AuthProvider } from '../components/Auth';
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AuthProvider>
+    <div>
+      <Head>
+        <title>Sand Beach Capital</title>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
+      <AuthProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthProvider>
+    </div>
   );
 }
 
