3d1e9fd4fc24ea54f24f745b36d83f0d0fbe5e68
Bryan Grill <grillb@gmail.com>
2021-01-13 05:57:31 -0500

    start login form

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import Section from './Section';
+import Header from './Header';
 export interface AuthContextModel {
   token: string | null;
-  logout: () => void
+  logout: () => void;
 }
 
 export const AuthContext = React.createContext<AuthContextModel | null>(null);
@@ -13,30 +14,49 @@ interface AuthProviderProps {
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
   const [token, setToken] = React.useState(null);
+  const [showLogin, setShowLogin] = React.useState(false);
 
   const logout = () => {
     if (typeof window !== 'undefined') {
-      localStorage.removeItem('token')
+      localStorage.removeItem('token');
       setToken(null);
     }
-  }
+  };
 
   React.useEffect(() => {
     if (typeof window !== 'undefined') {
       const jwt = localStorage.getItem('token');
-      console.log(jwt);
       setToken(jwt);
+      if (!jwt) setShowLogin(true);
+      return;
     }
+    setShowLogin(true);
   }, []);
 
   const value: AuthContextModel = {
     token,
-    logout
+    logout,
   };
 
   if (token) {
     return (
       <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+    );
+  }
+
+  if (showLogin) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <div className="container mx-auto bg-green-500 shadow rounded-md p-5">
+          <Header content="Login" size="lg" />
+          <input
+            placeholder="Enter the password"
+            className="border  rounded-md py-2 px-3 text-grey-darkest focus:outline-none focus:ring focus:border-blue-300"
+            name="password"
+            type="password"
+          />
+        </div>
+      </div>
     );
   }
 
