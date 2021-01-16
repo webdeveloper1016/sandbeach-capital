bcddfd8bcbaddb184aa096b565af37cf23eeff4d
Bryan Grill <grillb@gmail.com>
2021-01-16 05:49:35 -0500

    update env vars for vercel

--- pages/api/portfolio.ts
+++ pages/api/portfolio.ts
@@ -3,13 +3,10 @@ import data from './data';
 
 const prod = process.env.NODE_ENV === 'production';
 
-// https://nextjs.org/docs/basic-features/environment-variables
 const iex = {
-  env: prod ? 'Live' : 'Sandbox',
-  token: prod ? process.env.API_TOKEN : process.env.SB_API_TOKEN,
-  baseUrl: prod
-    ? 'https://cloud.iexapis.com/stable'
-    : 'https://sandbox.iexapis.com/stable',
+  env: process.env.IEX_API_URL.includes('sandbox') ? 'Sandbox' : 'Live',
+  token: process.env.IEX_API_TOKEN,
+  baseUrl: process.env.IEX_API_URL,
 };
 
 const handler = (req, res) => {
@@ -23,20 +20,16 @@ const handler = (req, res) => {
       const token = req.headers.authorization.split(' ')[1];
 
       // verify token
-      jwt.verify(
-        token,
-        process.env.JWT_SECRET,
-        (err) => {
-          if (err) {
-            res.status(401).send(prod ? 'Unauthorized' : JSON.stringify(err));
-            return;
-          }
-          // send response if no error
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.end(JSON.stringify({ data, iex }));
-        },
-      );
+      jwt.verify(token, process.env.JWT_SECRET, (err) => {
+        if (err) {
+          res.status(401).send(prod ? 'Unauthorized' : JSON.stringify(err));
+          return;
+        }
+        // send response if no error
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ data, iex }));
+      });
     } else {
       res.status(401).send('Unauthorized');
     }
