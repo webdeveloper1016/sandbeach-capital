06f70d5dd0a2b5678f93308f5e216b185765c706
Bryan Grill <grillb@gmail.com>
2021-01-18 06:35:56 -0500

    add svg icons

---
+++ components/Icons.tsx
@@ -0,0 +1,18 @@
+/** Icons via https://heroicons.com/ */
+
+export const Burger = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    className="h-8 w-8 text-gray-500"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M4 6h16M4 12h16M4 18h16"
+    />
+  </svg>
+);
