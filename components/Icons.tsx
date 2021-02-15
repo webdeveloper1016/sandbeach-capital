fde656902025e95f82a8eb9f4ddcaeaed0b29fc1
Bryan Grill <grillb@gmail.com>
2021-02-15 06:06:53 -0500

    add crypto menu item

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -103,6 +103,25 @@ export const Watchlist = () => (
   </div>
 );
 
+export const Crypto = () => (
+  <div className={baseContainerClass}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className={baseSVGClass}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+      />
+    </svg>
+  </div>
+);
+
 export const Logout = () => (
   <div className={baseContainerClass}>
     <svg
