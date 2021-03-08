8f4edd2b0569524cd5ff60692cef341952bfbfbe
Bryan Grill <grillb@gmail.com>
2021-03-08 17:15:23 -0500

    add todo

--- pages/api/account.ts
+++ pages/api/account.ts
@@ -17,6 +17,8 @@ const iex: IexUrlModel = {
   baseUrl: process.env.IEX_API_URL,
 };
 
+
+// TODO: add key stats --> https://iexcloud.io/docs/api/#key-stats
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     // check auth before proceeding
