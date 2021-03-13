a4d4ce9dae6329a98d425de1aa17728d931cb632
Bryan Grill <grillb@gmail.com>
2021-03-13 15:35:25 -0500

    add all table

--- pages/all.tsx
+++ pages/all.tsx
@@ -1,5 +1,47 @@
-const AllAccountsPage = () => {
-  return <div>all accounts listed here</div>;
-};
+import PortfolioData from '../components/PortfolioData';
+import { AccountTable } from '../components/AccountTable';
+import { SymbolNameCell, PercChangeCell } from '../components/TableCells';
+import { PercChangeModel } from '../ts';
+
+const AllAccountsPage = () => (
+  <PortfolioData>
+    {(data) => (
+      <div>
+        <div className="pb-4">
+          <AccountTable
+            columns={[
+              {
+                Header: 'Symbol',
+                accessor: 'symbolCompany',
+                Cell: (instance: {
+                  value: { symbol: string; name: string };
+                }) => <SymbolNameCell value={instance.value} />,
+              },
+              { Header: 'Price', accessor: 'prices.latest.display' },
+              {
+                Header: 'Day',
+                accessor: 'changePercent',
+                Cell: (instance: { value: PercChangeModel }) => (
+                  <PercChangeCell value={instance.value} />
+                ),
+              },
+              { Header: 'Equity', accessor: 'equity.display' },
+              { Header: 'Weight', accessor: 'weight.display' },
+              { Header: 'Shares', accessor: 'shares' },
+              { Header: 'Volume', accessor: 'volume.current.display' },
+              {
+                Header: 'Market Cap',
+                accessor: 'stats.marketCap.display',
+                style: { minWidth: '135px' },
+              },
+              { Header: 'YTD', accessor: 'stats.ytdChange.display' },
+            ]}
+            data={data.aggregatedHoldings}
+          />
+        </div>
+      </div>
+    )}
+  </PortfolioData>
+);
 
 export default AllAccountsPage;
