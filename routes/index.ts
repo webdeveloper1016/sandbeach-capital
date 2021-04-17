6cffa4992137145b500a72c8ab7c84185141593e
Bryan Grill <grillb@gmail.com>
2021-04-17 07:27:55 -0400

    reorder sidebar menu

--- routes/index.ts
+++ routes/index.ts
@@ -2,8 +2,8 @@ import { accountRoute } from '../config';
 
 export const links = [
   { href: '/', label: 'Home', icon: 'home' },
-  { href: '/accounts', label: 'Accounts', icon: 'accounts' },
-  { href: '/all', label: 'All', icon: 'chart' },
-  { href: `/account/${accountRoute}`, label: 'Watchlists', icon: 'watchlist' },
   { href: '/account/crypto', label: 'Crypto', icon: 'crypto' },
+  { href: `/account/${accountRoute}`, label: 'Watchlists', icon: 'watchlist' },
+  { href: '/all', label: 'All', icon: 'chart' },
+  { href: '/accounts', label: 'Accounts', icon: 'accounts' },
 ];
